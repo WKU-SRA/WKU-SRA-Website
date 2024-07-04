b4da1f50575f21f24a2696a4b7f2eaa73efd5988
AlbertZhaoCA <Zhaoq@kean.edu>
2024-07-04 22:15:44 +0800

    Add Loading component for improved user feedback during data fetch
    This commit introduces the Loading component, which is designed to enhance user experience by providing immediate visual feedback during data fetching or other asynchronous operations. The component features a centralized 'Loading' message styled with bold and prominent typography, ensuring visibility and clarity for users awaiting content loading. This addition aims to address issues related to user perception of unresponsive or slow interfaces by clearly indicating that data processing or retrieval is in progress.

---
+++ src/app/aboutus/loading.tsx
@@ -0,0 +1,7 @@
+export default function  Loading() {
+    return (
+        <div className="flex flex-col items-center justify-center h-full space-y-8">
+            <h1 className="text-6xl font-bold text-primary">Loading</h1>
+        </div>
+    )
+};
