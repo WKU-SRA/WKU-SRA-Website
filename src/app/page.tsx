9049bb615badf6d6176b4bdcc89f8ac1f3c06cbb
AlbertZhaoCA <Zhaoq@kean.edu>
2024-07-04 20:20:16 +0800

    feat(home): Add introduction section with hero height
    Implemented an introduction section on the home page, utilizing a hero height design to create a visually impactful first impression. Adjusted the layout and styling to ensure the introduction aligns with the overall aesthetic of the site. The hero height enhances the prominence of the welcome message and sets a welcoming tone for visitors.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 import React from "react";
-
-
-
-export default function Home(){
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import Intro from "@/components/HomeIntro";
+ 
+export default function Home() {
   return (
-  <div className="flex justify-center">
-  hello
-  </div>);
+    <div className="minh-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-0 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+        温州肯恩大学科技科研协会
+        </h1>
+        <p></p>
+        
+        <Intro />
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
 }
