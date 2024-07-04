883f573699bb5607145178918e4874c03394b501
AlbertZhaoCA <Zhaoq@kean.edu>
2024-07-04 20:20:16 +0800

    feat(home): Add introduction section with hero height
    Implemented an introduction section on the home page, utilizing a hero height design to create a visually impactful first impression. Adjusted the layout and styling to ensure the introduction aligns with the overall aesthetic of the site. The hero height enhances the prominence of the welcome message and sets a welcoming tone for visitors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ const {
 } = require("tailwindcss/lib/util/flattenColorPalette");
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
+const svgToDataUri = require("mini-svg-data-uri");
+
+
 
 
 const config = {
@@ -86,6 +89,18 @@ const config = {
   },
   plugins: [
     addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
   ],
 } satisfies Config
 
